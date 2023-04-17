e3ce5b80f891ce94ef31369b69394626f794a827
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-17 10:51:27 -0700

    added timer and start button functionality

--- javascript.js
+++ javascript.js
@@ -1,16 +1,37 @@
 var startButton = document.getElementById('start-btn');
 startButton.addEventListener('click', startQuiz);
+var questionContainer = document.querySelector(".question-container");
+questionContainer.style.display = "none";
 
+// this function reveals the questions and begins timer upon pressing start button.
 function startQuiz() {
-    console.log("begin quiz")
+    questionContainer.style.display = "block";
+    countDown();
     }
 
-//
+    // Timer and Countdown (still need to add penalty, where timer subtracts when an answer is wrong)
+    var timeEl = document.getElementById('count');
+    function countDown() {
+        var timeLeft = 60;
+        var timeInterval = setInterval(function() {
+       if(timeLeft > 1) {
+        timeEl.textContent = timeLeft + 'seconds remaing';
+        timeLeft--;
+       }
+       else if(timeLeft === 1) {
+        timeEl.textContent = timeLeft + 'seconds remaining';
+        timeLeft--;
+       }
+       else if(timeLeft < 1) {
+       timeEl.textContent = '';
+    
+       clearInterval(timeInterval);}
+        }, 1000);
+    }
+    
+ //   
 
-var output = document.getElementById("output");
-
-var optionButton = document.getElementsByClassName("option-btn");
-optionButton.addEventListener('click', Answer);
+// var output = document.getElementById("output");
 
 
 function Answer() {
@@ -20,7 +41,20 @@ function Answer() {
 function showQuestion(questions){
 
 }
+// working with question and answer container
 
+
+
+
+var optionButton = document.querySelectorAll("option-btn");
+    optionButton.addEventListener('click', Answer);
+
+ questionContainer.addEventListener('click', function(event))
+var element = event.target;
+
+
+
+ 
 var questions = {
     
         question1: "What does CSS stand for?",
@@ -29,9 +63,12 @@ var questions = {
     
 } 
 
+if (answer1){
 
-// var questionContainer = document.querySelector(".question-container");
-   // questionContainer.style.display = "none";
+}
+
+
+
 
     //use querySelectorAll make sure to loop thru the whole array so all buttons work 
 
